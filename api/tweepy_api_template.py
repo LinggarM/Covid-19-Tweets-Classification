688dd386e5013b40454d9337645d99032fd040b8
linggarm <linggarmc@gmail.com>
2023-12-24 15:50:04 +0700

    Add api template

---
+++ api/tweepy_api_template.py
@@ -0,0 +1,14 @@
+def api_key() :
+	return "API_KEY"
+
+def api_secret_key() :
+	return "API_SECRET_KEY"
+	
+def bearer_token() :
+	return "BEARER_TOKEN"
+
+def access_token() :
+	return "ACCESS_TOKEN"
+	
+def access_token_secret() :
+	return "ACCESS_TOKEN_SECRET"
